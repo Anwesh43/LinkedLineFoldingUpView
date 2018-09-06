988f1f217247aecce68e5e33469e5cd042b2c3b9
Anwesh43 <anweshthecool0@gmail.com>
2018-09-06 10:56:18 +0530

    adding Animator class

--- linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
+++ linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
@@ -47,4 +47,31 @@ class LineFoldingUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun animnate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
