ced23e6896d686e03f659ae4ff1e9b577e68c5db
Anwesh43 <anweshthecool0@gmail.com>
2018-09-06 10:49:58 +0530

    importing libraries and defining nodes

--- linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
+++ linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.linefoldingupview
 /**
  * Created by anweshmishra on 06/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
+
