69f42cff8ea092d137e4e98dba90eed1ca7e598a
Anwesh43 <anweshthecool0@gmail.com>
2018-09-06 11:20:19 +0530

    adding static create method

--- app/src/main/java/com/anwesh/uiprojects/linkedlinefoldingupview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinefoldingupview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinefoldingupview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linefoldingupview.LineFoldingUpView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineFoldingUpView.create(this)
     }
 }
