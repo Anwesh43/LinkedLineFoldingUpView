5fe7a7abb405160acb150d914a06f14cd6c3e8fc
Anwesh43 <anweshthecool0@gmail.com>
2018-09-06 11:11:51 +0530

    adding LineFoldingUp class

--- linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
+++ linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
@@ -137,4 +137,27 @@ class LineFoldingUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineFoldingUp(var i : Int) {
+        private val root : LFUNode = LFUNode(0)
+        private var curr : LFUNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+    }
 }
