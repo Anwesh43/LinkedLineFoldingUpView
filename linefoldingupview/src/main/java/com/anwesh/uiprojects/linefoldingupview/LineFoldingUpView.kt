535307e1b9f4d437b7ed06c1bb099e45da50570c
Anwesh43 <anweshthecool0@gmail.com>
2018-09-06 11:05:15 +0530

    adding drawLFUNode method

--- linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
+++ linefoldingupview/src/main/java/com/anwesh/uiprojects/linefoldingupview/LineFoldingUpView.kt
@@ -9,8 +9,29 @@ import android.view.MotionEvent
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.content.Context
+import android.graphics.Color
 
 val nodes : Int = 5
+
+fun Canvas.drawLFUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#0D47A1")
+    val gap : Float = (w) / (nodes + 1)
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    save()
+    translate(gap/3 + gap * i + gap/2, 0.9f * h - sc2 * (0.9f * h - gap/2))
+    for (j in 0..1) {
+        save()
+        rotate(90f * -sc1 * (1 - 2 * j))
+        drawLine(0f, 0f, gap/2 * (1 - 2 * j), 0f, paint)
+        restore()
+    }
+    restore()
+}
 
 class LineFoldingUpView(ctx : Context) : View(ctx) {
 
